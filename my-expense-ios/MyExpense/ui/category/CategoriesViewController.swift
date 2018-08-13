ba28571f53de1397b13ce660ed5d79097ab32965
Phyo Htet Arkar <phyohtetarkar@gmail.com>
2018-08-13 17:14:15 +0630

    add categories controller

---
+++ my-expense-ios/MyExpense/ui/category/CategoriesViewController.swift
@@ -0,0 +1,63 @@
+//
+//  CategoriesViewController.swift
+//  MyExpense
+//
+//  Created by Phyo Htet Arkar on 8/13/18.
+//  Copyright © 2018 Phyo Htet Arkar. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoriesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var context: NSManagedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    private lazy var fetchedResultController: NSFetchedResultsController<Category> = {
+        
+        let fetchRequest: NSFetchRequest<Category> = Category.fetchRequest()
+        
+        let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        
+        return controller
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view datasource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fetchedResultController.fetchedObjects?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cellCategory", for: indexPath) as? CategoryTableViewCell else {
+            fatalError("Invalid cell")
+        }
+        
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ my-expense-ios/MyExpense/ui/category/CategoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryTableViewCell.swift
+//  MyExpense
+//
+//  Created by OP-Macmini3 on 8/13/18.
+//  Copyright © 2018 Phyo Htet Arkar. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
